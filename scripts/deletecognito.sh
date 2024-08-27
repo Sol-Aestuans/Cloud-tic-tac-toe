32874ab651f19dfff770fc59f44e1d5bbf74ace2
Aidan <79548729+Sol-Aestuans@users.noreply.github.com>
2024-08-26 18:00:02 -0700

    Deleting old scripts directory

--- scripts/deletecognito.sh
+++
@@ -1,3 +0,0 @@
-echo "Deleting Amazon Cognito User Pool"
-POOL=$(aws cognito-idp delete-user-pool \
-  --user-pool-id ${USER_POOL_ID})
--- scripts/update-lambda.sh
+++
@@ -1,2 +0,0 @@
-zip -rq application.zip application/
-aws lambda update-function-code --function-name turn-based-api --zip-file fileb://application.zip
